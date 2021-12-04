596def07c7b8f0d29c8bce650622a3575d10ffc8
PJ Wilt <pj@assemblylegal.com>
2021-12-04 12:53:17 -0500

    Rearranging

---
+++ 1-sonar-sweep/sonar-1.py
@@ -0,0 +1,19 @@
+#! /bin/python3
+
+def sweep(depths: list) -> str:
+    increases = 0
+    if depths:
+        previous_depth = depths[0]
+        for depth in depths[1:]:
+            if depth > previous_depth:
+                increases = increases + 1
+            previous_depth = depth
+
+    return increases
+
+
+if __name__ == '__main__':
+    with open('input.txt') as file:
+        depths = [int(line.strip()) for line in file]
+
+    print(sweep(depths))
