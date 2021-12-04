be963026ac5f156fd03f185cb8d0486104f2ddfd
PJ Wilt <pj@assemblylegal.com>
2021-12-04 13:11:52 -0500

    Dive pt 2, this time with 100% more aim

---
+++ 2-dive/dive-2.py
@@ -0,0 +1,25 @@
+#! /bin/python3
+
+def navigate(commands: list) -> str:
+    x = 0
+    y = 0
+    aim = 0
+
+    if commands:
+        for command in commands:
+            if command[0] == "forward":
+                x = x + int(command[1])
+                y += aim * int(command[1])
+            elif command[0] == "down":
+                aim = aim + int(command[1])
+            elif command[0] == "up":
+                aim = aim - int(command[1])
+
+    return str(x * y)
+
+
+if __name__ == '__main__':
+    with open('input.txt') as file:
+        commands = [line.split(' ') for line in file]
+
+    print(navigate(commands))
