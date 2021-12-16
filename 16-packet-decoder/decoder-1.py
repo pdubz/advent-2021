d1ca9f1e24a991923574e6cd7704cb5b804f2154
PJ Wilt <pj@assemblylegal.com>
2021-12-16 17:48:20 -0500

    Packet decoder pt 1

---
+++ 16-packet-decoder/decoder-1.py
@@ -0,0 +1,87 @@
+#! /bin/python3
+
+class BinaryBuffer:
+    def __init__(self, hex):
+        self.location = 0
+        self.buffer = BinaryBuffer._bin_my_hex(hex)
+
+    @staticmethod
+    def _bin_my_hex(hex):
+        binary_string = ""
+        converter = {
+            '0': '0000',
+            '1': '0001',
+            '2': '0010',
+            '3': '0011',
+            '4': '0100',
+            '5': '0101',
+            '6': '0110',
+            '7': '0111',
+            '8': '1000',
+            '9': '1001',
+            'A': '1010',
+            'B': '1011',
+            'C': '1100',
+            'D': '1101',
+            'E': '1110',
+            'F': '1111'
+        }
+        for char in hex:
+            binary_string += converter[char]
+
+        return binary_string
+
+    def read(self, bits):
+        if self.location >= len(self.buffer):
+            return 'nope'
+        read_bits = self.buffer[self.location:self.location + bits]
+        self.location += bits
+        return int(read_bits, 2)
+
+    def peek(self):
+        return self.buffer[self.location:]
+
+
+class BitsDecoder:
+    def __init__(self, buffer: BinaryBuffer):
+        self.version_sum = 0
+        self.buffer = buffer
+        self._evaluate_packet()
+
+    def _evaluate_packet(self):
+        version = self.buffer.read(3)
+        type = self.buffer.read(3)
+        self.version_sum += version
+
+        if type == 4:
+            value = 0
+            keep_reading = True
+            while keep_reading:
+                keep_reading = bool(self.buffer.read(1))
+                value += self.buffer.read(4)
+            return value
+        else:
+            subpackets = []
+            operator_length_type = self.buffer.read(1)
+            if operator_length_type == 1:
+                packet_count = self.buffer.read(11)
+                for iteration in range(packet_count):
+                    subpackets.append(self._evaluate_packet())
+            else:
+                length = self.buffer.read(15)
+                buffer_start = self.buffer.location
+                while self.buffer.location < buffer_start + length:
+                    subpackets.append(self._evaluate_packet())
+
+
+
+def decode(input):
+    decoder = BitsDecoder(BinaryBuffer(input))
+    return decoder.version_sum
+
+
+if __name__ == '__main__':
+    with open('input.txt') as file:
+        value = file.readline().strip()
+
+    print(decode(value))
